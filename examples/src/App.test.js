4ea884e61975303b35b8e21b239e3920790d3598
CLacaile <clementlacaile@gmail.com>
2024-12-24 13:22:30 +0100

    Added a simple test that checks if react-org-chart div is present

--- examples/src/App.test.js
+++ examples/src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+test('renders App without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(App, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+test('contains a react-org-chart div', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  expect(div.innerHTML).toContain('<div id=\"react-org-chart\"></div>');
   ReactDOM.unmountComponentAtNode(div);
 });
